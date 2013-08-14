15460b727d0df7cd2e5ac354ecb75389ec0ff0cb
gnarlinsky@gmail.com <me@me>
2013-08-14 17:11:25 -0500

    initial db stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# no config for now
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
-from app import views
+from app import views, models
--- config.py
+++ config.py
@@ -1,4 +1,11 @@
-CSRF_ENABLED = False
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
+# path to database file
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# path to dir that stores the SQLAlchemy-migrate data files
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+CSRF_ENABLED = False
 # Though set CSRF_ENABLED to False, still error if no SECRET_KEY....
 SECRET_KEY = 'foobar'
